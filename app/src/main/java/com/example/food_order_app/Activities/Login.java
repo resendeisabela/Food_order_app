0deb5cb5c19ee2d2a3cdceb868cd7385842ea681
Isabela Resende <isabelares13@gmail.com>
2023-06-23 15:07:13 -0400

    add show password checkbox

--- app/src/main/java/com/example/food_order_app/Activities/Login.java
+++ app/src/main/java/com/example/food_order_app/Activities/Login.java
@@ -1,14 +1,26 @@
 package com.example.food_order_app.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.food_order_app.MainActivity;
 import com.example.food_order_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -27,9 +39,56 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        edtEmail = findViewById(R.id.edtEmail);
-//        edtSenha = findViewById(R.id.edtSenha);
-//        btnLogin = findViewById(R.id.btnLogin);
-//        btnCadastro = findViewById(R.id.btnCadastro);
+        fAuth = FirebaseAuth.getInstance();
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCadastro = findViewById(R.id.btnCadastro);
+        loginPB = findViewById(R.id.loginPB);
+        mostrarSenha = findViewById(R.id.mostarSenha);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginEmail = edtEmail.getText().toString();
+                String loginSenha = edtSenha.getText().toString();
+
+                if(!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha)){
+                    loginPB.setVisibility(View.VISIBLE);
+                    fAuth.signInWithEmailAndPassword(loginEmail, loginSenha)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        telaPrincipal();
+                                    }
+                                    else {
+                                        String error = task.getException().getMessage();
+                                        Toast.makeText(Login.this, ""+error, Toast.LENGTH_SHORT).show();
+                                        loginPB.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                            }); 
+                }
+            }
+        });
+
+        mostrarSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    edtSenha.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else{
+                    edtSenha.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+
+    private void telaPrincipal() {
+        Intent intent  = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/food_order_app/MainActivity.java
+++ app/src/main/java/com/example/food_order_app/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.food_order_app.Activities.Login;
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,11 +13,25 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth fAuth;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fAuth = FirebaseAuth.getInstance();
+        logout = findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                finish();
+            }
+        });
+
     }
 
     @Override
